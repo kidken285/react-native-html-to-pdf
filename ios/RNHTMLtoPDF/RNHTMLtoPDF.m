8d900c8870111d57a4743e2d2b452867487e9c1a
KEN <nguyenvinhtuan.2805@gmail.com>
2022-01-01 14:29:37 +0700

    Update RNHTMLtoPDF.m
    - update function

--- ios/RNHTMLtoPDF/RNHTMLtoPDF.m
+++ ios/RNHTMLtoPDF/RNHTMLtoPDF.m
@@ -224,4 +224,27 @@ RCT_EXPORT_METHOD(convert:(NSDictionary *)options
     }
 }
 
+- (NSString *)stringByEvaluatingJavaScriptFromString:(NSString *)script {
+    __block NSString *resultString = nil;
+    __block BOOL finished = NO;
+
+    [_webView evaluateJavaScript:script completionHandler:^(id result, NSError *error) {
+        if (error == nil) {
+            if (result != nil) {
+                resultString = [NSString stringWithFormat:@"%@", result];
+            }
+        } else {
+            NSLog(@"evaluateJavaScript error : %@", error.localizedDescription);
+        }
+        finished = YES;
+    }];
+
+    while (!finished)
+    {
+        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
+    }
+
+    return resultString;
+}
+
 @end
